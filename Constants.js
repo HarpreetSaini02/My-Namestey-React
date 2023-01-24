9d45a128329971fa2b2d0b16688e0a617e2717b9
HarpreetSaini02 <121703700+HarpreetSaini02@users.noreply.github.com>
2023-01-24 11:55:25 +0530

    Header and Routing

---
+++ Constants.js
@@ -0,0 +1,1 @@
+export const HEADER_IMG_URL = "https://lh3.googleusercontent.com/Em7AHf7XBH_RtGfCBVXz9RH8SM_pHkj3xPP-yd3cRguY1_Jc8fmqgx6WxnvGVyPV5xs5gL3HCD0FCuv6Xo4CwoY6ak4=w100-rw";
---
+++ src/components/Header.js
@@ -0,0 +1,39 @@
+import { HEADER_IMG_URL } from "../../Constants";
+import { Link } from "react-router-dom";
+import AboutUs from "./AboutUs";
+import Home from "./Home";
+import Contact from "./Contact";
+export const Title = () => {
+  return (
+    <a href="/">
+      <img className="logo" alt="logo" src={HEADER_IMG_URL} />
+    </a>
+  );
+};
+
+const Header = () => {
+  return (
+    <div className="header">
+      <Title />
+
+      <div className="nav-items">
+        <ul>
+          <Link to="/home">
+            <li>Home</li>
+          </Link>
+          <Link to="/about">
+            <li>About Us</li>
+          </Link>
+          <Link to="/contact">
+            <li>Contact</li>
+          </Link>
+          
+         
+          <li>Cart</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
