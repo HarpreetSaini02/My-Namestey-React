40dc79f22798d38b47cf475a51b9d99129fd06c6
HarpreetSaini02 <121703700+HarpreetSaini02@users.noreply.github.com>
2023-02-03 18:13:59 +0530

    Lifting State Up

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,47 @@
+import { useState } from "react";
+import { FAQ } from "../../Constants";
 
+const Section = ({ title, description ,isVisible,setIsVisible}) => {
+ // const [isVisible, setIsVisible] = useState(false);
+  return (
+    <div className="footer">
+      <h1> {title} </h1>
+      {isVisible ? (
+        <button className="btn" onClick={() => setIsVisible(false)}>-</button>
+      ) : (
+        <button className="btn" onClick={() => setIsVisible(true)}>+</button>
+      )}
+     { isVisible && <p>{description}</p>}
+    </div>
+  );
+};
 const Footer = () => {
-    return(
-
-        <h1> Footer </h1>
-    );
-}
+    const [visibleSection, setVisibleSection] = useState("");
+  return (
+    <div className="footer-sections">
+      <h1 className="footer-tittle"> FAQ'S </h1>
+      {/* {FAQ.map((question)
+      =>{ return()})} */}
+      {FAQ.map((question)=>{
+        return(
+            <Section key={question.id} id={question.id} title={question.title} description={question.description}
+          isVisible={visibleSection === question.id }
+          setIsVisible={(display) => {
+            if(display) {
+              setVisibleSection(question.id);
+            } else {
+              setVisibleSection(" ");
+            }
+          }
+          } />
+        )
+      
+      }
+      )
+      }
+      
+    </div>
+  );
+};
 
 export default Footer;
--- src/components/instamart.js
+++ src/components/instamart.js
@@ -1,8 +1,50 @@
-const Instamart = () => {
+import { useState } from "react";
+const Section = ({ tittle, description,isVisible, setIsVisible}) => {
+  // const [isVisible, setIsVisible] = useState(false);
+  return (
+    <div className="section">
+      <h3 className="tittle"> {tittle}</h3>
+      {isVisible ? (
+        <button onClick={() => setIsVisible(false)}>Hide</button>
+      ) : (
+        <button onClick={() => setIsVisible(true)}>Show</button>
+      )}
+      {isVisible && <p> {description}</p>}
+    </div>
+  );
+};
 
-    return (
-        <h1> This is instamart</h1>
-    )
-}
+const Instamart = () => {
+  const [visibleSection, setVisibleSection] = useState("about");
+  return (
+    <div>
+      <h1> InstaMart </h1>
+      <Section
+        tittle={"AboutInstaMart"}
+        description={
+          "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like"
+        }
+        isVisible = {visibleSection === "about"}
+        setIsVisible= {()=>setVisibleSection(visibleSection === "about"? "" : "about")}
+      />
+      <Section
+        tittle={"TeamInstaMart"}
+        description={
+          "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like"
+        }
+        isVisible = {visibleSection === "team"}
+        setIsVisible= {()=>setVisibleSection(visibleSection === "team"? "" : "team")}
+      />
+      <Section
+        tittle={"CareerInstaMart"}
+        description={
+          "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like"
+        }
+        isVisible = {visibleSection === "career"}
+        setIsVisible={() => setVisibleSection(visibleSection === "career" ? "" : "career")}
+      />
+    </div>
+  );
+};
 
 export default Instamart;
